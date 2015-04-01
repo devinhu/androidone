85b0c5ebdb509323a241981122662738568df6a7
devinhu <wosunmo@gmail.com>
2015-04-01 15:44:41 +0800

    xiugai

--- oneCore/src/com/sd/core/utils/NToast.java
+++ oneCore/src/com/sd/core/utils/NToast.java
@@ -21,7 +21,7 @@ import android.widget.Toast;
 public class NToast {
 	
 	public static void shortToast(Context context, int resId) {
-		showToast(context, context.getString(resId), Toast.LENGTH_SHORT);
+		showToast(context, resId, Toast.LENGTH_SHORT);
 	}
 	
 	public static void shortToast(Context context, String text) {
@@ -31,13 +31,26 @@ public class NToast {
 	}
 
 	public static void longToast(Context context, int resId) {
-		showToast(context, context.getString(resId), Toast.LENGTH_LONG);
+		showToast(context, resId, Toast.LENGTH_LONG);
 	}
 	
 	public static void longToast(Context context, String text) {
 		if(!TextUtils.isEmpty(text) && !"".equals(text.trim())){
 			showToast(context, text, Toast.LENGTH_LONG);
 		}
+	}
+	
+	public static void showToast(Context context, int resId, int duration) {
+		if (context == null){
+			return;
+		}
+		if (context != null && context instanceof Activity) {
+	        if(((Activity) context).isFinishing()) {
+	            return;
+	        }
+		}
+		String text = context.getString(resId);
+		showToast(context, text, duration);
 	}
 	
 	public static void showToast(Context context, String text, int duration) {
